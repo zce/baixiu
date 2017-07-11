e78fb544023990914d4edf318c1632ac4942d789
zce <w@zce.me>
2017-07-11 15:30:51 +0800

    step-05 抽离公共部分（sidebar）

--- admin/profile.php
+++ admin/profile.php
@@ -76,43 +76,7 @@
     </div>
   </div>
 
-  <div class="aside">
-    <div class="profile">
-      <img class="avatar" src="/static/uploads/avatar.jpg">
-      <h3 class="name">布头儿</h3>
-    </div>
-    <ul class="nav">
-      <li>
-        <a href="index.php"><i class="fa fa-dashboard"></i>仪表盘</a>
-      </li>
-      <li>
-        <a href="#menu-posts" class="collapsed" data-toggle="collapse">
-          <i class="fa fa-thumb-tack"></i>文章<i class="fa fa-angle-right"></i>
-        </a>
-        <ul id="menu-posts" class="collapse">
-          <li><a href="posts.php">所有文章</a></li>
-          <li><a href="post-add.php">写文章</a></li>
-          <li><a href="categories.php">分类目录</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="comments.php"><i class="fa fa-comments"></i>评论</a>
-      </li>
-      <li>
-        <a href="users.php"><i class="fa fa-users"></i>用户</a>
-      </li>
-      <li>
-        <a href="#menu-settings" class="collapsed" data-toggle="collapse">
-          <i class="fa fa-cogs"></i>设置<i class="fa fa-angle-right"></i>
-        </a>
-        <ul id="menu-settings" class="collapse">
-          <li><a href="nav-menus.php">导航菜单</a></li>
-          <li><a href="slides.php">图片轮播</a></li>
-          <li><a href="settings.php">网站设置</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
+  <?php include 'inc/sidebar.php' ;?>
 
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
