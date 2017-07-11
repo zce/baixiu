499a702b6ec8c292636ebabced91a0ae49b83fe1
zce <w@zce.me>
2017-07-11 14:43:26 +0800

    step-03 项目配置文件

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 项目配置文件
+ */
+
+/**
+ * 数据库主机
+ */
+define('DB_HOST', '127.0.0.1');
+
+/**
+ * 数据库用户名
+ */
+define('DB_USER', 'root');
+
+/**
+ * 数据库密码
+ */
+define('DB_PASS', 'wanglei');
+
+/**
+ * 数据库名称
+ */
+define('DB_NAME', 'baixiu');
--- index.php
+++ index.php
@@ -3,5 +3,8 @@
  * 网站前台脚本文件
  */
 
+// 载入配置文件
+require 'config.php';
 ?>
 <h1>前台</h1>
+<p><?php echo DB_HOST; ?></p>
