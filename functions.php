6e917eaf454c2c8be595ca14d6ac679cdab3f32d
zce <w@zce.me>
2017-07-12 11:44:41 +0800

    step-16 抽象创建数据库连接函数

--- functions.php
+++ functions.php
@@ -7,18 +7,28 @@
 require_once '../config.php';
 
 /**
- * 执行一个查询语句，返回查询到的数据（关联数组混合索引数组）
- * @param  string $sql 需要执行的查询语句
- * @return array       查询到的数据（二维数组）
+ * 根据配置文件信息创建一个数据库连接，注意用完以后需要关闭
+ * @return mysqli 数据库连接对象
  */
-function xiu_query ($sql) {
-  // 建立数据库连接
+function xiu_connect () {
   $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
   if (!$connection) {
     // 如果连接失败报错
     die('<h1>Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() . '</h1>');
   }
+
+  return $connection;
+}
+
+/**
+ * 执行一个查询语句，返回查询到的数据（关联数组混合索引数组）
+ * @param  string $sql 需要执行的查询语句
+ * @return array       查询到的数据（二维数组）
+ */
+function xiu_query ($sql) {
+  // 获取数据库连接
+  $connection = xiu_connect();
 
   // 定义结果数据容器，用于装载查询到的数据
   $data = array();
