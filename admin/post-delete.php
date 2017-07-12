c62b3cec689a8ed6f9445db3bfce6e2d2e3d46f4
zce <w@zce.me>
2017-07-12 19:37:43 +0800

    step-36 post-delete.php 支持批量删除

--- admin/post-delete.php
+++ admin/post-delete.php
@@ -7,7 +7,7 @@ require '../functions.php';
 
 if (!empty($_GET['id'])) {
   // 拼接 SQL 并执行
-  xiu_execute(sprintf('delete from posts where id = %d', $_GET['id']));
+  xiu_execute(sprintf('delete from posts where id in (%s)', $_GET['id']));
 }
 
 // 获取删除后跳转到的目标链接，优先跳转到来源页面，否则默认跳转到列表页（第一页、未筛选）
