151fda4ef46d1b90ee214865e3e566afcda33353
zce <w@zce.me>
2017-07-12 11:29:49 +0800

    step-14 后台统计数据查询（封装查询函数）

--- admin/index.php
+++ admin/index.php
@@ -24,22 +24,59 @@ if (empty($_SESSION['is_logged_in'])) {
 // 查询数据
 // ========================================
 
+/**
+ * 执行一个查询语句，返回查询到的数据（关联数组混合索引数组）
+ * @param  string $sql 需要执行的查询语句
+ * @return array       查询到的数据（二维数组）
+ */
+function xiu_query ($sql) {
+  // 建立数据库连接
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  if (!$connection) {
+    // 如果连接失败报错
+    die('<h1>Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() . '</h1>');
+  }
+
+  // 定义结果数据容器，用于装载查询到的数据
+  $data = array();
+
+  // 执行参数中指定的 SQL 语句
+  if ($result = mysqli_query($connection, $sql)) {
+    // 查询成功，则获取结果集中的数据
+
+    // 遍历每一行的数据
+    while ($row = mysqli_fetch_array($result)) {
+      // 追加到结果数据容器中
+      $data[] = $row;
+    }
+
+    // 释放结果集
+    mysqli_free_result($result);
+  }
+
+  // 关闭数据库连接
+  mysqli_close($connection);
+
+  // 返回容器中的数据
+  return $data;
+}
+
 // 查询文章总数
+$post_count = xiu_query('select count(1) from posts')[0][0];
+// select count(1) 查询出来的永远是单行单列的数据，所以[0][0]
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// 查询草稿总数
+$drafted_count = xiu_query('select count(1) from posts where status = \'drafted\'')[0][0];
 
-if (!$connection) {
-  die('<h1>Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() . '</h1>');
-}
+// 查询分类总数
+$category_count = xiu_query('select count(1) from categories')[0][0];
 
-if ($result = mysqli_query($connection, 'select count(1) from posts')) {
-  $post_count = mysqli_fetch_row($result)[0];
-  mysqli_free_result($result);
-}
+// 查询评论总数
+$comment_count = xiu_query('select count(1) from comments')[0][0];
 
-mysqli_close($connection);
-
-// 其余几个查询只是查询语句不通，所以这里不列举了。
+// 查询待审核的评论总数
+$held_count = xiu_query('select count(1) from comments where status = \'held\'')[0][0];
 
 ?>
 <!DOCTYPE html>
@@ -77,9 +114,9 @@ mysqli_close($connection);
               <h3 class="panel-title">站点内容统计：</h3>
             </div>
             <ul class="list-group">
-              <li class="list-group-item"><strong><?php echo isset($post_count) ? $post_count : ' 查询失败 '; ?></strong>篇文章（<strong>2</strong>篇草稿）</li>
-              <li class="list-group-item"><strong>6</strong>个分类</li>
-              <li class="list-group-item"><strong>5</strong>条评论（<strong>1</strong>条待审核）</li>
+              <li class="list-group-item"><strong><?php echo $post_count; ?></strong>篇文章（<strong><?php echo $drafted_count; ?></strong>篇草稿）</li>
+              <li class="list-group-item"><strong><?php echo $category_count; ?></strong>个分类</li>
+              <li class="list-group-item"><strong><?php echo $comment_count; ?></strong>条评论（<strong><?php echo $held_count; ?></strong>条待审核）</li>
             </ul>
           </div>
         </div>
