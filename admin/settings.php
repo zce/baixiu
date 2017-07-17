bc12857b304ccbe68ba12628ca342d19ee24694f
zce <w@zce.me>
2017-07-17 09:50:56 +0800

    step-84 保存设置选项

--- admin/settings.php
+++ admin/settings.php
@@ -14,6 +14,38 @@ require '../functions.php';
 // 获取登录用户信息
 xiu_get_current_user();
 
+// 处理表单请求
+// ========================================
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (!empty($_POST['site_logo'])) {
+    xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'site_logo\'', $_POST['site_logo']));
+  }
+  if (!empty($_POST['site_name'])) {
+    xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'site_name\'', $_POST['site_name']));
+  }
+  if (!empty($_POST['site_description'])) {
+    xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'site_description\'', $_POST['site_description']));
+  }
+  if (!empty($_POST['site_keywords'])) {
+    xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'site_keywords\'', $_POST['site_keywords']));
+  }
+
+  xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'comment_status\'', !empty($_POST['comment_status'])));
+
+  xiu_execute(sprintf('update `options` set `value` = \'%s\' where `key` = \'comment_reviewed\'', !empty($_POST['comment_reviewed'])));
+}
+
+// 查询数据
+// ========================================
+
+$data = xiu_query('select * from options');
+$options = array();
+
+foreach ($data as $item) {
+  $options[$item['key']] = $item['value'];
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -34,25 +66,21 @@ xiu_get_current_user();
       <button class="btn btn-default navbar-btn fa fa-bars"></button>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="profile.php"><i class="fa fa-user"></i>个人中心</a></li>
-        <li><a href="login.php"><i class="fa fa-sign-out"></i>退出</a></li>
+        <li><a href="logout.php"><i class="fa fa-sign-out"></i>退出</a></li>
       </ul>
     </nav>
     <div class="container-fluid">
       <div class="page-title">
         <h1>网站设置</h1>
       </div>
-      <!-- 有错误信息时展示 -->
-      <!-- <div class="alert alert-danger">
-        <strong>错误！</strong> 发生XXX错误
-      </div> -->
-      <form class="form-horizontal">
+      <form class="form-horizontal" action="/admin/settings.php" method="post">
         <div class="form-group">
           <label for="site_logo" class="col-sm-2 control-label">网站图标</label>
           <div class="col-sm-6">
             <input id="site_logo" name="site_logo" type="hidden">
             <label class="form-image">
-              <input id="logo" type="file">
-              <img src="/static/assets/img/logo.png">
+              <input id="upload" type="file">
+              <img src="<?php echo $options['site_logo']; ?>">
               <i class="mask fa fa-upload"></i>
             </label>
           </div>
@@ -60,29 +88,29 @@ xiu_get_current_user();
         <div class="form-group">
           <label for="site_name" class="col-sm-2 control-label">站点名称</label>
           <div class="col-sm-6">
-            <input id="site_name" name="site_name" class="form-control" type="type" placeholder="站点名称">
+            <input id="site_name" name="site_name" class="form-control" type="type" value="<?php echo $options['site_name']; ?>" placeholder="站点名称">
           </div>
         </div>
         <div class="form-group">
           <label for="site_description" class="col-sm-2 control-label">站点描述</label>
           <div class="col-sm-6">
-            <textarea id="site_description" name="site_description" class="form-control" placeholder="站点描述" cols="30" rows="6"></textarea>
+            <textarea id="site_description" name="site_description" class="form-control" placeholder="站点描述" cols="30" rows="6"><?php echo $options['site_description']; ?></textarea>
           </div>
         </div>
         <div class="form-group">
           <label for="site_keywords" class="col-sm-2 control-label">站点关键词</label>
           <div class="col-sm-6">
-            <input id="site_keywords" name="site_keywords" class="form-control" type="type" placeholder="站点关键词">
+            <input id="site_keywords" name="site_keywords" class="form-control" type="type" value="<?php echo $options['site_keywords']; ?>" placeholder="站点关键词">
           </div>
         </div>
         <div class="form-group">
           <label class="col-sm-2 control-label">评论</label>
           <div class="col-sm-6">
             <div class="checkbox">
-              <label><input id="comment_status" name="comment_status" type="checkbox" checked>开启评论功能</label>
+              <label><input id="comment_status" name="comment_status" type="checkbox"<?php echo $options['comment_status'] ? ' checked' : ''; ?>>开启评论功能</label>
             </div>
             <div class="checkbox">
-              <label><input id="comment_reviewed" name="comment_reviewed" type="checkbox" checked>评论必须经人工批准</label>
+              <label><input id="comment_reviewed" name="comment_reviewed" type="checkbox"<?php echo $options['comment_reviewed'] ? ' checked' : ''; ?>>评论必须经人工批准</label>
             </div>
           </div>
         </div>
@@ -100,6 +128,34 @@ xiu_get_current_user();
 
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
+  <script>
+    $(function () {
+      // 异步上传文件
+      $('#upload').on('change', function () {
+        // 选择文件后异步上传文件
+        var formData = new FormData()
+        formData.append('file', $(this).prop('files')[0])
+
+        // 上传图片
+        $.ajax({
+          url: '/admin/upload.php',
+          cache: false,
+          contentType: false,
+          processData: false,
+          data: formData,
+          type: 'post',
+          success: function (res) {
+            if (res.success) {
+              $('#site_logo').val(res.data)
+              $('#upload').siblings('img').attr('src', res.data).fadeIn()
+            } else {
+              alert('上传文件失败')
+            }
+          }
+        })
+      })
+    })
+  </script>
   <script>NProgress.done()</script>
 </body>
 </html>
